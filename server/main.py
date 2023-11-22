75de86d19d3dd1237f4a2b5d59ddeeed6c25d964
root <root@ee-tik-vm054.ethz.ch>
2023-11-22 22:26:03 +0100

    fixed front query

--- server/main.py
+++ server/main.py
@@ -1,5 +1,4 @@
 from typing import Union
-from search import que
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pymilvus import connections, db, utility
@@ -15,18 +14,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["*"],
+    allow_methods=["POST, PUT, PATCH, GET, DELETE, OPTIONS"],
     allow_headers=["*"],
     allow_credentials=True,
-)
-
-time.sleep(60)
-
-connections.connect(
-  alias="default",
-  host='milvus-standalone',
-  port='19530',
-  db_name="default"
 )
 
 search_params = {
@@ -36,18 +26,21 @@ search_params = {
     "params": {"nlist": 2048}
 }
 
+#time.sleep(60)
+
+connections.connect(
+  alias="default",
+  host='milvus-standalone',
+  port='19530',
+  db_name="default"
+)
+
 collection = Collection("DISCO") 
 collection.load()
 
-def int16_to_float32(x):
-    return (x / 32767.0).astype(np.float32)
-
-def float32_to_int16(x):
-    x = np.clip(x, a_min=-1., a_max=1.)
-    return (x * 32767.).astype(np.int16)
-
 model = laion_clap.CLAP_Module(enable_fusion=False, amodel= 'HTSAT-base')
 model.load_ckpt('music_speech_audioset_epoch_15_esc_89.98.pt') # download the default pretrained checkpoint.
+
 
 @app.get("/")
 def read_root():
@@ -58,7 +51,26 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 @app.get("/search")
-def search(query, collection):
+def search(query):
     x = que(query)
     print("ANAN", x)
     return x
+
+def que (query: str):
+    text_data = [query, "ROCK"] 
+    text_embed = model.get_text_embedding(text_data)
+    results = collection.search(
+        data=[text_embed[0]],
+        anns_field="audio_embedding_spotify", 
+        # the sum of `offset` in `param` and `limit` 
+        # should be less than 16384.
+        param=search_params,
+        limit=10,
+        expr=None,
+        # set the names of the fields you want to 
+        # retrieve from the search result.
+        output_fields=['video_url_youtube'],
+        consistency_level="Strong"
+    )
+
+    return [hit.entity.get('video_url_youtube') for hits in results for hit in hits]
