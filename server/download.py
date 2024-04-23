a335594703dba5e2c4e504a111acbe9b51418cdc
root <root@ee-tik-vm054.ethz.ch>
2024-04-24 01:00:16 +0200

    deletion of unnecessary files

--- server/download.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-from huggingface_hub import login
-
-print("TEST")
-login(token="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")
-DATASET = 'DISCOX/DISCO-10M'
-dataset = load_dataset(DATASET)
-dataset.save_to_disk('./DISCO-10M')
